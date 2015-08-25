b3219e6926c28b070222eea12be722bdc8e30c4f
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-08-25 16:38:01 +0200

    Make Node members public.

--- node/Node.h
+++ node/Node.h
@@ -2,13 +2,18 @@
 #define I3DOA_NODE_H
 
 namespace doa {
+
     template <class T>
     class Node {
+
+    public:
         Node(T i = 0, Node* n = nullptr)
                 : info(i), next(n) {};
         T info;
         Node* next;
+
     };
+
 }
 
 #endif //I3DOA_NODE_H
