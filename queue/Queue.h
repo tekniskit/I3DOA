ba44fb589d916a9d81e1461457f2c7e4b2d618cf
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-09-01 14:04:56 +0200

    Implement queue.

---
+++ queue/Queue.h
@@ -0,0 +1,49 @@
+#ifndef I3DOA_QUEUE_H
+#define I3DOA_QUEUE_H
+
+#include "../LLToolkit/LLToolkit.h"
+
+namespace doa {
+
+    template<class T>
+    class Queue {
+    public:
+        void push(T element) {
+            if (isEmpty()) {
+                llToolkit.headInsert(headPtr, element);
+                endPtr = headPtr;
+            }
+            else {
+                llToolkit.insert(endPtr, element);
+                endPtr = endPtr->next;
+            }
+        }
+
+        void pop() {
+            llToolkit.headRemove(headPtr);
+        }
+
+        T top() {
+            if (isEmpty())
+                throw "Stack is empty";
+
+            return headPtr->info;
+        }
+
+        bool isEmpty() {
+            return headPtr == nullptr;
+        }
+
+        void print() {
+            llToolkit.print(headPtr);
+        }
+
+    private:
+        Node<T> *headPtr = nullptr;
+        Node<T> *endPtr = nullptr;
+        LLToolkit<T> llToolkit;
+    };
+
+}
+
+#endif //I3DOA_QUEUE_H
