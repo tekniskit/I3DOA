5f441bde110fd602f6c6e32e5c881f8f1a584d74
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-09-01 16:09:52 +0200

    Align the comments.

--- LLToolkit/LLToolkit.h
+++ LLToolkit/LLToolkit.h
@@ -10,15 +10,15 @@ namespace doa {
     class LLToolkit {
 
     public:
-        // Precondition: headPtr points to the head of a list
-        // Postcondition: Info has been inserted at the head of the list, and headPtr is updated
+        // Precondition: 	headPtr points to the head of a list
+        // Postcondition: 	Info has been inserted at the head of the list, and headPtr is updated
         // Time complexity: O(1)
         void headInsert(Node<T> *&headPtr, T info) {
             headPtr = new Node<T>(info, headPtr);
         }
 
-        // Precondition: headPtr points to the head of a list with at least 1 element
-        // Postcondition: The head element has been removed and headPtr is updated to point to the new head element
+        // Precondition: 	headPtr points to the head of a list with at least 1 element
+        // Postcondition: 	The head element has been removed and headPtr is updated to point to the new head element
         // Time complexity: O(1)
         void headRemove(Node<T> *&headPtr) {
             if (headPtr == nullptr) {
@@ -30,8 +30,8 @@ namespace doa {
             delete condemned;
         }
 
-        // Precondition: prevPtr points to Node just before insertion point
-        // Postcondition: A new Node with data=info has been inserted into the list after prevPtr
+        // Precondition: 	prevPtr points to Node just before insertion point
+        // Postcondition: 	A new Node with data=info has been inserted into the list after prevPtr
         // Time complexity: O(1)
         void insert(Node<T> *prevPtr, T info) {
             if (prevPtr == nullptr) {
@@ -41,8 +41,8 @@ namespace doa {
             prevPtr->next = new Node<T>(info, prevPtr->next);
         }
 
-        // Precondition: prevPtr points to Node just before Node to remove
-        // Postcondition: The Node after prevPtr has been removed from the list
+        // Precondition: 	prevPtr points to Node just before Node to remove
+        // Postcondition: 	The Node after prevPtr has been removed from the list
         // Time complexity: O(1)
         void remove(Node<T> *prevPtr) {
             if (prevPtr == nullptr) {
@@ -54,8 +54,8 @@ namespace doa {
             delete condemned;
         }
 
-        // Precondition: sourcePtr is the head pointer of a linked list.
-        // Postcondition: A pointer to a copy of the linked list is returned. The original list is unaltered.
+        // Precondition: 	sourcePtr is the head pointer of a linked list.
+        // Postcondition: 	A pointer to a copy of the linked list is returned. The original list is unaltered.
         // Time complexity: O(N)
         Node<T> *copy(Node<T> *sourcePtr) {
             if (sourcePtr == nullptr) {
@@ -73,8 +73,8 @@ namespace doa {
             return head;
         }
 
-        // Precondition: headPtr is the head pointer of a linked list.
-        // Postcondition: All nodes of the list have been deleted, and the headPtr is NULL.
+        // Precondition: 	headPtr is the head pointer of a linked list.
+        // Postcondition: 	All nodes of the list have been deleted, and the headPtr is NULL.
         // Time complexity: O(N)
         void clear(Node<T> *&headPtr) {
             while (headPtr != nullptr) {
@@ -82,8 +82,8 @@ namespace doa {
             }
         }
 
-        // Precondition: headPtr is the head pointer of a linked list.
-        // Postcondition: The data item of each Node in the list has been printed to the screen.
+        // Precondition: 	headPtr is the head pointer of a linked list.
+        // Postcondition: 	The data item of each Node in the list has been printed to the screen.
         // Time complexity: O(N)
         void print(Node<T> *headPtr) {
             for (Node<T> *node = headPtr; node != nullptr; node = node->next) {
@@ -93,8 +93,8 @@ namespace doa {
             std::cout << "/" << std::endl;
         }
 
-        // Precondition: headPtr is the head pointer of a linked list.
-        // Postcondition: headPtr points to the start of a list that is reversed with respect to the original list
+        // Precondition: 	headPtr is the head pointer of a linked list.
+        // Postcondition: 	headPtr points to the start of a list that is reversed with respect to the original list
         // Time complexity: O(N)
         void reverse(Node<T> *&headPtr) {
             if (headPtr == nullptr) {
@@ -115,8 +115,8 @@ namespace doa {
             headPtr->next = left;
         }
 
-        // Precondition: splitPtr points to the node before the split point
-        // Postcondition: A pointer is returned that points to the first node after splitPtr. The original list ends at the node pointed to by splitPtr
+        // Precondition: 	splitPtr points to the node before the split point
+        // Postcondition: 	A pointer is returned that points to the first node after splitPtr. The original list ends at the node pointed to by splitPtr
         // Time complexity: O(1)
         Node<T> *split(Node<T> *splitPtr) {
             if (splitPtr == nullptr) {
@@ -128,8 +128,8 @@ namespace doa {
             return head;
         }
 
-        // Precondition: values points to an array of at least size n
-        // Postcondition: A pointer is returned that points to the head of a list in which the nodes contains values from the array values
+        // Precondition: 	values points to an array of at least size n
+        // Postcondition: 	A pointer is returned that points to the head of a list in which the nodes contains values from the array values
         // Time complexity: O(N)
         Node<T> *build(T *values, size_t n) {
             if (n == 0) {
@@ -145,8 +145,8 @@ namespace doa {
             return head;
         }
 
-        // Precondition: head1 and head2 each point to the head of linked lists
-        // Postcondition: head1 points to a list containing head1-lists' elements followed by head2-lists element.
+        // Precondition: 	head1 and head2 each point to the head of linked lists
+        // Postcondition: 	head1 points to a list containing head1-lists' elements followed by head2-lists element.
         // Time complexity: O(N)
         void join(Node<T> *&head1, Node<T> *head2) {
             if (head2 == nullptr) {
@@ -167,8 +167,8 @@ namespace doa {
             end->next = head2;
         }
 
-        // Precondition: head points to the head of a linked list
-        // Postcondition: The list is rotated left by once - if it was 1-2-3-4, it is now 2-3-4-1
+        // Precondition: 	head points to the head of a linked list
+        // Postcondition: 	The list is rotated left by once - if it was 1-2-3-4, it is now 2-3-4-1
         // Time complexity: O(N)
         void rotateLeft(Node<T> *&head) {
             if (head == nullptr) {
@@ -188,8 +188,8 @@ namespace doa {
             head = newHead;
         }
 
-        // Precondition: head points to the head of a linked list
-        // Postcondition: The list is rotated right once - if it was 1-2-3-4, it is now 4-1-2-3
+        // Precondition: 	head points to the head of a linked list
+        // Postcondition: 	The list is rotated right once - if it was 1-2-3-4, it is now 4-1-2-3
         // Time complexity: O(N)
         void rotateRight(Node<T> *&head) {
             if (head == nullptr) {
