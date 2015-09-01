4686406896cd6d85978c918cade3438266eb2468
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-09-01 16:32:09 +0200

    Add descriptions of PriorityQueue methods.

--- queue/PriorityQueue.h
+++ queue/PriorityQueue.h
@@ -10,10 +10,16 @@ namespace doa {
     template<class T>
     class PriorityQueue {
     public:
+        // Precondition:
+        // Postcondition:   The vector is resized to fit the required priorities.
+        // Time complexity: O(N)
         PriorityQueue(unsigned int noPriorities = 1) {
             priorities.resize(noPriorities);
         }
 
+        // Precondition:    The priority is below the number of priorities specified in the ctor.
+        // Postcondition:   Element is added with the specified priority.
+        // Time complexity: O(1)
         void push(T element, unsigned int priority) {
             if (priority > priorities.size() - 1) {
                 throw "Invalid priority";
@@ -22,6 +28,9 @@ namespace doa {
             priorities[priority].push(element);
         }
 
+        // Precondition:
+        // Postcondition:   The front element is deleted.
+        // Time complexity: O(N)
         void pop() {
             for (int i = 0; i < priorities.size(); i++) {
                 if (!priorities[i].isEmpty()) {
@@ -31,6 +40,9 @@ namespace doa {
             }
         }
 
+        // Precondition:    Queue is not empty.
+        // Postcondition:   The front element is returned.
+        // Time complexity: O(N)
         T front() {
             if (isEmpty())
                 throw "Queue is empty";
@@ -42,6 +54,9 @@ namespace doa {
             }
         }
 
+        // Precondition:
+        // Postcondition:   Returns whether the queue is empty.
+        // Time complexity: O(N)
         bool isEmpty() {
             for (int i = 0; i < priorities.size(); i++) {
                 if (!priorities[i].isEmpty())
@@ -51,6 +66,9 @@ namespace doa {
             return true;
         }
 
+        // Precondition:
+        // Postcondition:   Prints the queue to standard out.
+        // Time complexity: O(N^2)
         void print() {
             for (int i = 0; i < priorities.size(); i++) {
                 priorities[i].print();
